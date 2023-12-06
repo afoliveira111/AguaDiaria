11a0365d40c05a3116f95e46c438a6788c19972d
Felipe <felipetermometria@hotmail.com>
2023-12-06 18:31:13 -0300

    Create SetOnClickListener for 3 buttons

--- app/src/main/java/com/example/aguadiaria/MainActivity.kt
+++ app/src/main/java/com/example/aguadiaria/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.aguadiaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_small).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btn_medium).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btn_large).setOnClickListener {
+
+        }
     }
 }
